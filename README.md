8f20d809240ee3cdb7a54dc09bc371026b009f1e
Kira Belo <doeva2016@yandex.ru>
2022-06-30 16:33:11 +0300

    brain-gcd

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 <a href="https://codeclimate.com/github/KiraBelo/frontend-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/59d8475196089f39db8b/maintainability" /></a>
 https://asciinema.org/a/eC0sciUKUnJIunIM2vB5KxTTP
 https://asciinema.org/a/p7RSUStfl1jOLKjwne9H70GBK
+https://asciinema.org/a/E0BecEG0kXKXOKSjyMsgYOemn
---
+++ src/games/game-gcd-logic.js
@@ -0,0 +1,21 @@
+import randomNumber from '../randomNumber.js';
+import gameEngine from '../index.js';
+
+const gameRule = 'Find the greatest common divisor of given numbers.';
+const getGcd = (number1, number2) => {
+  if (number2 > 0) {
+    return getGcd(number2, number1 % number2);
+  }
+  return number1;
+};
+
+const gameRound = () => {
+  const number1 = randomNumber(1, 50);
+  const number2 = randomNumber(1, 50);
+  const question = `${number1} ${number2}`;
+  const rightAnswer = `${getGcd(number1, number2)}`;
+  return [question, rightAnswer];
+};
+
+const brainGcd = () => gameEngine(gameRule, gameRound);
+export default brainGcd;
