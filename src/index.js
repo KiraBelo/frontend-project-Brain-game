97c3201cc6d353f422637fcaebf603aab93111e0
Kira Belo <doeva2016@yandex.ru>
2022-07-04 17:05:13 +0300

    error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const gameEngine = (gameRule, gameRound) => {
   console.log('Welcome to the Brain Games!');
-  const UserName = readlineSync.question('May I have your name? ');
+  const UserName = readlineSync.question('May I have your name?');
   console.log(`Hello, ${UserName}!`);
   console.log(gameRule);
 
