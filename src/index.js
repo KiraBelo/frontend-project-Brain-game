b5a10918ac715d76886e5a501a6fe88babb28c3b
Kira Belo <doeva2016@yandex.ru>
2022-07-04 17:33:38 +0300

    index fix troubles with _

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import readlineSync from 'readline-sync';
 
-const gameEngine = (gameRule, gameRound) => {
+  const roundCounts = 3;
+  
+  const gameEngine = (gameRule, gameRound) => {
   console.log('Welcome to the Brain Games!');
-  const UserName = readlineSync.question('May I have your name?');
+  const UserName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${UserName}!`);
   console.log(gameRule);
 
-  const roundCounts = 3;
   for (let i = 0; i < roundCounts; i += 1) {
     const [question, rightAnswer] = gameRound();
     console.log(`Question: ${question}`);
@@ -14,8 +15,8 @@ const gameEngine = (gameRule, gameRound) => {
     if (rightAnswer === userAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'`);
-      console.log(`Let's try again ${UserName}!`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${UserName}!`);
       return;
     }
   }
