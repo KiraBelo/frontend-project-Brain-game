fa56b1bbdd7cff46873671ffd61138463859bf10
Kira Belo <doeva2016@yandex.ru>
2022-08-01 13:09:20 +0300

    { => new line

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,7 +6,9 @@ const minRange = 1;
 const maxRange = 100;
 
 const isPrime = (x) => {
-  if (x < 2) { return false; }
+  if (x < 2) {
+    return false;
+  }
   for (let i = 2; i <= Math.sqrt(x); i += 1) {
     if (x % i === 0) {
       return false;
